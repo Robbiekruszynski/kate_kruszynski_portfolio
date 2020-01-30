bd3637e64b5c8ea00efa5c913256dea2a87019d1
robbie <robbiekruszynski@gmail.com>
2020-01-29 21:18:34 -0800

    add svg along with make change to banner scss

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
-const Banner = (props) => (
-    <section id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h1>Kate Kruszynski</h1>
-            </header>
-            <div className="content">
-                <p>A curation of travel experiences and design efforts.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">E X P L O R E </a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <h1>Kate Kruszynski</h1>
+      <div className="content">
+        <p>A curation of travel experiences and design efforts.</p>
+        <ul className="actions">
+          <li>
+            <a href="#one" className="button next scrolly">
+              E X P L O R E{' '}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 )
 
 export default Banner
